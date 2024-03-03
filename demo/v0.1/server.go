788c5c2bc9307765a893adc8a649967921d8615c
xypf <2992247892@qq.com>
2024-03-03 20:47:49 +0800

    feat: Update demo

--- demo/v0.1/server.go
+++ demo/v0.1/server.go
@@ -1,8 +1,47 @@
 package main
 
-import "github.com/cpf2021-gif/gos/tnet"
+import (
+	"fmt"
+
+	"github.com/cpf2021-gif/gos/tiface"
+	"github.com/cpf2021-gif/gos/tnet"
+)
+
+type PingRouter struct {
+	tnet.BaseRouter
+}
+
+// Test PreHandle
+func (pr *PingRouter) PreHandle(request tiface.IRequest) {
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("before ping... ping... ping...\n"))
+	if err != nil {
+		fmt.Println("call back before ping... ping... ping... error")
+	}
+}
+
+// Test Handle
+func (pr *PingRouter) Handle(request tiface.IRequest) {
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("ping... ping... ping...\n"))
+	if err != nil {
+		fmt.Println("call back ping... ping... ping... error")
+	}
+}
+
+// Test PostHandle
+func (pr *PingRouter) PostHandle(request tiface.IRequest) {
+	_, err := request.GetConnection().GetTCPConnection().Write([]byte("after ping... ping... ping...\n"))
+	if err != nil {
+		fmt.Println("call back after ping... ping... ping... error")
+	}
+}
 
 func main() {
+	// 创建一个server句柄
 	s := tnet.NewServer("[gos] Server v0.1")
+
+	// 配置路由
+	s.AddRouter(&PingRouter{})
+
+	// 开启服务
 	s.Serve()
 }
