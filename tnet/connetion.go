254cc716218e3e0f854f2c6088b2aecf87456dac
xypf <2992247892@qq.com>
2024-03-04 21:57:39 +0800

    feat: Add global config

--- tnet/connetion.go
+++ tnet/connetion.go
@@ -6,6 +6,7 @@ import (
 	"net"
 
 	"github.com/cpf2021-gif/gos/tiface"
+	"github.com/cpf2021-gif/gos/utils"
 )
 
 type Connection struct {
@@ -48,8 +49,8 @@ func (c *Connection) startReader() {
 	defer c.Stop()
 
 	for {
-		// 读取客户端的数据到buf中，最大512字节
-		buf := make([]byte, 512)
+		// 读取客户端的数据到buf中
+		buf := make([]byte, utils.GlobalConfig.Gos.MaxPacketSize)
 		_, err := c.Conn.Read(buf)
 
 		if err == io.EOF {
